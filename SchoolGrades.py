748b31cc3421409ed197abc4031421bcb0432c32
Ricardo E Escobar <93016916+RicardoEEJ@users.noreply.github.com>
2025-09-27 21:42:23 +0200

    Update SchoolGrades.py
    More clarity at the moment of printing the grade.

--- SchoolGrades.py
+++ SchoolGrades.py
@@ -1,15 +1,15 @@
 score = input("Enter Score: ")
 sc = float(score) 
-if sc>1.0 :
+if sc>10.0 :
     print ("error")
 else: 
-    if sc >= (0.9):
-        print("A")
-    elif sc >= (0.8): 
-        print("B")
-    elif sc >= (0.7):
-        print("C")
-    elif sc >= (0.6):
-        print("D")
-    elif sc < (0.6):
-        print ("F")
+    if sc >= (9.0):
+        print(f"your grade is : A")
+    elif sc >= (8.0): 
+        print(f"your grade is : B")
+    elif sc >= (7.0):
+        print(f"your grade is : C")
+    elif sc >= (6.0):
+        print(f"your grade is : D")
+    elif sc < (6.0):
+        print (f"your grade is : F")
